6e5e51c0835f93b640e37f43d0abf2d923869a65
gedaias jair bg <gedaias@geda.dev>
2023-09-03 10:21:42 -0600

    Placing the ship
    Create an object that contains the ships generated from the Ship class
    For each ship generate a random coordinate in the grid
    Validate if there is room in the board by calculating if the size of the ship is greater than the size of the grid
    If there is room the ship is placed in the grid
    End the loop and break

--- src/app/gameboard.js
+++ src/app/gameboard.js
@@ -1,3 +1,5 @@
+import Ship from './ship.js';
+
 export default class GameBoard {
   constructor(size) {
     this.size = size;
@@ -7,4 +9,49 @@ export default class GameBoard {
   generateBoard() {
     return new Array(this.size).fill(null).map(() => new Array(this.size).fill(null));
   }
+
+  placeShips() {
+    const ships = {
+      carrier: new Ship(5),
+      battleship: new Ship(4),
+      submarine: new Ship(3),
+      destroyer: new Ship(2),
+    };
+
+    Object.values(ships).forEach((ship) => {
+      let shipPlaced = false;
+
+      while (!shipPlaced) {
+        // Generate random coordinate in the grid
+        const lat = Math.floor(Math.random() * (this.size - 0));
+        const lon = Math.floor(Math.random() * (this.size - 0));
+
+        // Validate horizontal room
+        if (ship.length + lon > this.size) {
+          console.log('out of room');
+        } else {
+          for (let index = 0; index < ship.length; index++) {
+            this.board[lat][lon + index] = ship;
+          }
+          shipPlaced = true;
+          break;
+        }
+
+        // Validate vertical room
+        if (ship.length + lat > this.size) {
+          console.log('out of room');
+        } else {
+          for (let index = 0; index < ship.length; index++) {
+            this.board[lat + index][lon] = ship;
+          }
+          shipPlaced = true;
+          break;
+        }
+      }
+    });
+  }
 }
+
+const game = new GameBoard(5);
+game.placeShips();
+console.table(game.board);

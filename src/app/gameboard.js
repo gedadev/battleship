141cb73ed2dc5a091c3bcd0d0d8b503f03eaac33
gedaias jair bg <gedaias@geda.dev>
2023-09-04 22:39:52 -0600

    Reorganize validations
    Add a validation for the coordinate generated randomly to make sure it's free
    Add room and collision validation in the same if and remove the else statements
    Separate the collision validation for vertical and horizontal placing, try catch is not required anymore

--- src/app/gameboard.js
+++ src/app/gameboard.js
@@ -16,55 +16,53 @@ export default class GameBoard {
       carrier: new Ship(5),
       battleship: new Ship(4),
       submarine: new Ship(3),
-      destroyer: new Ship(2),
+      destroyer: new Ship(3),
+      patrolBoat: new Ship(2),
     };
 
     Object.values(ships).forEach((ship) => {
-      let shipPlaced = false;
-
-      while (!shipPlaced) {
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
         // Generate random coordinate in the grid
         const lat = Math.floor(Math.random() * (this.size - 0));
         const lon = Math.floor(Math.random() * (this.size - 0));
 
-        // Validate vertical room
-        if (ship.length + lat > this.size) {
-          console.log('out of room');
-        } else if (this.validateCollisions(ship.length, lat, lon)) {
-          console.log('you hit another ship');
-        } else {
-          for (let index = 0; index < ship.length; index++) {
-            this.board[lat + index][lon] = ship;
+        if (this.board[lat][lon] === null) {
+          // Validate vertical room
+          if (ship.length + lat <= this.size
+            && !this.validateVerticalCollisions(ship.length, lat, lon)) {
+            for (let index = 0; index < ship.length; index += 1) {
+              this.board[lat + index][lon] = ship.length;
+            }
+            break;
           }
-          shipPlaced = true;
-          break;
-        }
 
-        // Validate horizontal room
-        if (ship.length + lon > this.size) {
-          console.log('out of room');
-        } else if (this.validateCollisions(ship.length, lat, lon)) {
-          console.log('you hit another ship');
-        } else {
-          for (let index = 0; index < ship.length; index += 1) {
-            this.board[lat][lon + index] = ship;
+          // Validate horizontal room
+          if (ship.length + lon <= this.size
+            && !this.validateHorizontalCollisions(ship.length, lat, lon)) {
+            for (let index = 0; index < ship.length; index += 1) {
+              this.board[lat][lon + index] = ship.length;
+            }
+            break;
           }
-          shipPlaced = true;
-          break;
         }
       }
     });
   }
 
-  validateCollisions(shipLength, lat, lon) {
-    for (let index = 0; index < shipLength + 1; index += 1) {
-      try {
-        if ((this.board[lat][lon + index] !== null && this.board[lat + 1][lon + index] !== null)
-         || (this.board[lat + index][lon] !== null && this.board[lat + index][lon + 1] !== null)) {
-          return true;
-        }
-      } catch (error) {
-        console.log('no room');
+  validateVerticalCollisions(shipLength, lat, lon) {
+    for (let index = 0; index < shipLength; index += 1) {
+      if ((this.board[lat + index][lon] !== null)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  validateHorizontalCollisions(shipLength, lat, lon) {
+    for (let index = 0; index < shipLength; index += 1) {
+      if ((this.board[lat][lon + index] !== null)) {
+        return true;
       }
     }
     return false;

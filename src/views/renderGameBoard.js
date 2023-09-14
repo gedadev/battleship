523e4b5b3d7ac3a942d78e0c29e8421146b4a7d8
gedaias jair bg <gedaias@geda.dev>
2023-09-13 22:07:09 -0600

    Display the gameboard in the DOM
    Take as parameter a node list of the elements that contains the grids
    Take also an array with the 2 player objects
    Add other property to save the grids generated
    For each container create a 2d array with the size defined for the gameboard
    Loop trough the 2d array and for each iteration create a div element representing a pixel in the grid
    Add the classname defined in the css file, append it to the container and add the element to the current index of the arrays

---
+++ src/views/renderGameBoard.js
@@ -0,0 +1,25 @@
+export default class RenderGameBoard {
+  constructor(gridContainer, players) {
+    this.gridContainer = gridContainer;
+    this.player = players;
+    this.grid = [];
+  }
+
+  generateGrid() {
+    this.gridContainer.forEach((element, index) => {
+      const grid = new Array(this.player[index].gameBoard.size).fill(null)
+        .map(() => new Array(this.player[index].gameBoard.size).fill(null));
+
+      for (let i = 0; i < this.player[index].gameBoard.size; i += 1) {
+        for (let j = 0; j < this.player[index].gameBoard.size; j += 1) {
+          const pixel = document.createElement('div');
+
+          pixel.className = 'pixel';
+          element.appendChild(pixel);
+          grid[i][j] = pixel;
+        }
+      }
+      this.grid.push(grid);
+    });
+  }
+}
